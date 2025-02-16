57ca325a6c9919d442706df6f1cc2f3bdd471550
sayed <sayed@Sameer.mynetworksettings.com>
2025-02-16 15:22:09 -0500

    new code

---
+++ src/test/java/Demohw/Exercise1.java
@@ -0,0 +1,22 @@
+package Demohw;
+
+import utils.Hooks;
+
+public class Exercise1 extends Hooks{
+	public static void main(String[]args) {
+		
+		setUp("edge");
+		//navigate to the page
+		driver.get("");
+		
+		
+		
+		
+		
+		
+		
+		tearDown();
+	}
+	
+
+}
